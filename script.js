2e4b3025c11ed6bbc34b78aaf9de8bb175f42298
Thales Cardoso <canadathales@gmail.com>
2022-10-19 15:38:57 -0400

    Started styling the page.

--- script.js
+++ script.js
@@ -1,21 +1,60 @@
-document.getElementById("search").addEventListener('keyup',(e) => {
-    console.log(e.target.value);
+window.addEventListener('load', (e) => {
+   const input = document.getElementById('search')
+   input.value = ''
+})
 
-    const url = 'https://itunes.apple.com/search?media=music&limit=10&term=' + e.target.value
+
+document.getElementById("search").addEventListener('keyup',(e) => {
+
+    const url = 'https://itunes.apple.com/search?media=music&limit=12&term=' + e.target.value
 
     fetch(url)
     .then(response => response.json())
     .then(response => render(response.results))
+    .catch(err => {
+        renderError()
+        console.error(err)   
+    });
     
 })
 
 const render = (value) => {
-    let lista = document.getElementById('listagem')
+    let list = document.getElementById('listing')
 
-    lista.innerHTML = ''
+    list.innerHTML = ''
     
      value.forEach(element => {
-            lista.innerHTML += element.trackName + `<audio src="${element.previewUrl}" type="audio/x-m4a" controls></audio>` + '<br>'
+            list.innerHTML += 
+            
+            `
+            <div id="card">
+                <div class="cardUp">
+                    <img src='${element.artworkUrl100}'></img>
+                    <div class='artistInfo'>
+                        <div id="trackName">
+                            ${element.trackName}
+                        </div>
+                        <div id="artistName">
+                            ${element.artistName}
+                        </div>
+                    </div>    
+
+                    <button onclick="addToFav()" id='favBtn'><i class="fa fa-heart"></i></button>
+                
+                </div>         
+                <audio src="${element.previewUrl}" type="audio/x-m4a" controls></audio>
+            </div>`
         })
 
+}
+
+const renderError = () => {
+    let list = document.getElementById('listing')
+
+    list.innerHTML = 'I could not find any song :('
+
+}
+
+const addToFav = () => {
+    alert('added')
 }
